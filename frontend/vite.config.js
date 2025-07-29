d829d0ceb3b65d45304480fab0e4f99fe7858dad
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-07-29 13:59:21 +0530

    jszip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,13 @@ import { parse } from 'postcss';
 
 // https://vite.dev/config/
 // vite.config.js
+// vite.config.js
 export default {
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT, 10) || 3000
-  }
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['component-creator-1.onrender.com'],
+  },
 };
+
 
