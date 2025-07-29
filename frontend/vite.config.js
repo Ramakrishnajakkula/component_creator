3b13a8a4a72bf4663f26ece0dcd538b00d17c6b9
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-07-29 13:55:46 +0530

    jszip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { parse } from 'postcss';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT, 10) || 3000
+  }
+};
+
